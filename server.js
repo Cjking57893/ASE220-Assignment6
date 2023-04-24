3c3a0a0fe46de84e843ef9a303e97ec279831cd1
Cody <cody.king04@outlook.com>
2023-04-23 22:40:05 -0400

    POST working correctly

--- server.js
+++ server.js
@@ -51,26 +51,31 @@ async function remove(db, database, collection, document) {
     console.log(result);
 }
 
+app.post("/api/jsonBlob", async (req, res) => {
+    // Inserting document into JSONBlob collection and responding with result of request.
+	let result = await insert(db,'Assignment6','JSONBlob',req.body);
+    let objectID = result.insertedId.toString();
+    
+    // Setting status code to 201, location header to documentID, and sending back json in response.
+    res.statusCode = 201;
+    res.setHeader("Location", objectID);
+    res.json(req.body);
+});
+
 app.get("/post", async (req, res) => {
-    // Requesting the Posts collection and sending it back in the response.
+    // Requesting the JSONBlob collection and sending it back in the response.
     let result = await find(db, "Assignment6", "Posts", {});
     res.json(result);
 });
 
-app.post("/post", async (req, res) => {
-    // Inserting document into Posts collection and responding with result of request.
-	let result = await insert(db,'Assignment6','Posts',req.body);
-	res.json(result);
-});
-
 app.put("/post/:id", async (req, res) => {
-    // Updating document in Posts collection and responding with result of request.
-    let result = await update(db, "Assignment6", "Posts", {"_id": new ObjectID(req.params.id)}, {$set: req.body});
+    // Updating document in JSONBlob collection and responding with result of request.
+    let result = await update(db, "Assignment6", "JSONBlob", {"_id": new ObjectID(req.params.id)}, {$set: req.body});
     res.json(result);
 });
 
 app.delete("/post/:id", async (req, res) => {
-    // Deleting document in Posts collection and responding with result of request.
+    // Deleting document in JSONBlob collection and responding with result of request.
     let result = await remove(db, "Assignment6", "Posts", {"_id": new ObjectID(req.params.id)});
     res.json(result);
 });
